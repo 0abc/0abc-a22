073f24d76b85b095d13d38e703119c532dabf4de
Bijvoets <bbijvoets@mail.com>
2018-02-21 12:43:14 +0100

    common-api

--- simulation/ai/common-api/entity.js
+++ simulation/ai/common-api/entity.js
@@ -77,7 +77,7 @@ m.Template = m.Class({
     if (techRequired === "phase_city")
       return 3;
     if (techRequired === "phase_metropolis")
-      return 3;
+      return 4;
     return 0;
   },
 
